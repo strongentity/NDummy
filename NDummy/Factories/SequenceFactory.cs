fdad321817985fdc226b8fb367495ab125e39e3d
Anton Setiawan <antonmaju@gmail.com>
2012-03-21 20:54:42 +0700

    Added interfaces

---
+++ NDummy/Factories/SequenceFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NDummy.Factories
+{
+    public class SequenceFactory<T> : IFactory<T>
+    {
+        public T Generate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
