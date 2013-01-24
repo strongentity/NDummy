8148138b34ca2669c3c2667954cf0e489b574885
Anton Setiawan <antonmaju@gmail.com>
2013-01-24 23:41:03 +0700

    Add enum factory

--- NDummy.Tests/CustomTypes/CustomEnum.cs
+++ NDummy.Tests/CustomTypes/CustomEnum.cs
@@ -4,7 +4,9 @@
     {
         Value1=1,
         Value2,
-        Value3
+        Value3,
+        Value4,
+        Value5
     }
 
     public enum CustomBitFlag
---
+++ NDummy/Factories/EnumFactory.cs
@@ -0,0 +1,37 @@
+﻿namespace NDummy.Factories
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class EnumFactory<T> : IFactory<T> where T:struct, IConvertible
+    {
+        private readonly IList<T> values = new List<T>();
+        private int index = 0;
+    
+        public EnumFactory()
+        {
+            var array = Enum.GetValues(typeof (T));
+
+            foreach (T item in array)
+            {
+                values.Add(item);
+            }
+        }
+
+        public T Generate()
+        {
+            if (index >= values.Count)
+                index = 0;
+            
+            var result = values[index];
+            index++;
+
+            return result;
+        }
+
+        object IFactory.Generate()
+        {
+            return Generate();
+        }
+    }
+}
